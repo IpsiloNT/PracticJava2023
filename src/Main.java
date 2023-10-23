2f5adf80a7743dad26b7ac17e08b156dfb32d846
Lobanov Kirill <ipsilont954@gmail.com>
2023-10-23 15:21:46 +0300

    убрал лишние импорты

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-import java.io.*;
-
-//import com.google.gson.JsonArray;
-//import com.google.gson.JsonElement;
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-//import com.google.gson.Gson;
-//import com.google.gson.GsonBuilder;
 
 class ConsoleMenu {
     private static final Scanner scanner = new Scanner(System.in);
